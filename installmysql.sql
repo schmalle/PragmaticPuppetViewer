a1430bb6327635dcfa5aa3f0c7aae4103bd2e2df
Markus Schmall <schmall.markus@googlemail.com>
2014-12-06 17:07:44 +0100

    added nice buttons instead of bad status texts

--- installmysql.sql
+++ installmysql.sql
@@ -15,6 +15,8 @@ CREATE TABLE Nodes(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 
 INSERT INTO Nodes VALUES (1, "testnode", "firstseen", "lastseen", "127.0.0.1", "ok", "puppetversion", "configversion");
 INSERT INTO Nodes VALUES (2, "testnode_orig", "firstseen", "lastseen", "127.0.0.1", "ok", "puppetversion", "configversion");
+INSERT INTO Nodes VALUES (3, "testnode_changed", "firstseen", "lastseen", "127.0.0.1", "changed", "puppetversion", "configversion");
+INSERT INTO Nodes VALUES (4, "testnode_failed", "firstseen", "lastseen", "127.0.0.1", "failed", "puppetversion", "configversion");
 
 
 GRANT ALL PRIVILEGES
